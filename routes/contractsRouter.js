92db3dc9230e1d23f3d5fbea14394bc92c90b30d
Aguspepee <sanchezagustinezequiel@gmail.com>
2022-04-30 19:46:40 -0300

    Cambios generales

--- routes/contractsRouter.js
+++ routes/contractsRouter.js
@@ -4,7 +4,11 @@ var router = express.Router();
 const contractsController = require("../controllers/contractsController.js")
 
 /* GET users listing. */
-router.get('/', contractsController.getAll);
-router.post('/', contractsController.createContract);
+router.get('/', contractsController.getAll); 
+router.get('/list', contractsController.getList);
+router.get('/names', contractsController.getNames);
+router.post('/', contractsController.create);
+router.put('/edit/:id', contractsController.edit);
+router.delete('/:id', contractsController.delete);
 
 module.exports = router;
