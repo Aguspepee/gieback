4550f9cf495192c563d483f51f01306ff3a7a07e
Aguspepee <sanchezagustinezequiel@gmail.com>
2022-06-05 22:15:22 -0300

    Refactory de PartesGetRestricted

--- controllers/partesController.js
+++ controllers/partesController.js
@@ -31,206 +31,60 @@ module.exports = {
             page: req.query.page,
             limit: req.query.rowsPerPage,
         };
-
-
-        const search = {
-            "numero_reporte": req.query["numero_reporte"] || "",
-            "tag": req.query["tag"] || "",
-            "items.0.descripcion_servicio": req.query["items.0.descripcion_servicio"] || "",
-            "items.0.cantidad": req.query["items.0.cantidad"] || "",
-            "inspector": req.query["inspector"] || "",
-            "numero_orden": req.query["numero_orden"] || "",
-            "cliente": req.query["cliente"] || "",
-            "contrato": req.query["contrato"] || "",
-            "unidad": req.query["unidad"] || "",
-            "fecha_carga": req.query["fecha_carga"] || "",
-            "semana_carga": req.query["semana_carga"] || "",
-            "fecha_inspeccion": req.query["fecha_inspeccion"] || "",
-            "AAMM_inspeccion": req.query["AAMM_inspeccion"] || "",
-            "AAsem_inspeccion": req.query["AAsem_inspeccion"] || "",
-            "semana_inspeccion": req.query["semana_inspeccion"] || "",
-            "items.0.codigo_servicio": req.query["items.0.codigo_servicio"] || "",
-            "archivo": req.query["archivo"] || "",
-            //Campos Booleanos
-            "trabajo_terminado": bTA.booleanToArray(req.query["trabajo_terminado"]) || [true, false],
-            "informe_realizado": bTA.booleanToArray(req.query["informe_realizado"]) || [true, false],
-            "informe_revisado": bTA.booleanToArray(req.query["informe_revisado"]) || [true, false],
-            "remito_realizado": bTA.booleanToArray(req.query["remito_realizado"]) || [true, false],
-            "certificado_realizado": bTA.booleanToArray(req.query["certificado_realizado"]) || [true, false],
-        }
-        console.log(search)
         var sort = {};
         sort[req.query.orderBy.replace("[", ".").replace("]", "")] = req.query.order === 'asc' ? -1 : 1;
-        console.log("SORT", sort)
         try {
             const documents = await partesModel.aggregate([
-                //Se extrae el campo tipo_actividad para poder concatenarlo
                 {
                     $addFields: {
-                        tipo_actividad: {
-                            $first: {
-                                $map: {
-                                    input: "$items",
-                                    as: "r",
-                                    in: { $toString: "$$r.tipo_actividad" }
-                                }
-                            }
-                        }
-                    }
-                },
-                //Se crea el el campo mes_inspección_word para poder concatenarlo
-                {
-                    $addFields: {
-                        mes_inspeccion_word: {
-                            $let: {
-                                vars: {
-                                    monthsInString: ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC']
-                                },
-                                in: {
-                                    $arrayElemAt: ['$$monthsInString', { '$month': '$fecha_inspeccion' }]
-                                }
-                            }
-                        }
-                    }
-                },
-                //Se crea el el campo dia_inspección para poder concatenarlo
-                {
-                    $addFields: { dia_inspeccion: { '$toString': { '$dayOfMonth': '$fecha_inspeccion' } } }
-                },
-
-                {
-                    '$project': {
-                        'tipo_actividad': 1,
-                        'numero_reporte': 1,
-                        'numero_orden': 1,
-                        'descripcion_equipo': 1,
-                        'inspector': 1,
-                        'tag': 1,
-                        'tag_detalle': 1,
-                        'contrato': 1,
-                        'cliente': 1,
-                        'área': 1,
-                        'unidad': 1,
-                        'fecha_carga': 1,
-                        'remito_realizado': 1,
-                        'remito_numero': 1,
-                        'remito_fecha': 1,
-                        'certificado_realizado': 1,
-                        'certificado_fecha': 1,
-                        'archivo': {
-                            $concat: ["$unidad", "_", "$numero_reporte", "_", "$tipo_actividad", "_", "$tag", "_", "$mes_inspeccion_word", "$dia_inspeccion"]
-                        },
-                        'modificado': 1,
-                        'modificado_fecha': 1,
-                        'modificado_nombre': 1,
-                        'semana_carga': {
-                            $toString: { '$isoWeek': '$fecha_carga' }
-                        },
-                        'fecha_inspeccion': 1,
-                        'dia_inspeccion': 1,
-                        'semana_inspeccion': {
-                            $toString: { '$isoWeek': '$fecha_inspeccion' }
-                        },
-                        'mes_inspeccion': {
-                            $toString: { '$month': '$fecha_inspeccion' }
-                        },
-                        'AAsem_inspeccion': {
-                            $concat: [
+                        semana_carga: { $toString: { '$isoWeek': '$fecha_carga' } },
+                        semana_inspeccion: { $toString: { '$isoWeek': '$fecha_inspeccion' } },
+                        mes_inspeccion: { $toString: { '$month': '$fecha_inspeccion' } },
+                        AAsem_inspeccion: { $concat: [{ $toString: { $year: "$fecha_inspeccion" } }, "/", { $toString: { $isoWeek: "$fecha_inspeccion" } }] },
+                        AAMM_inspeccion: { $concat: [{ $toString: { $year: "$fecha_inspeccion" } }, "/", { $toString: { $month: "$fecha_inspeccion" } }] },
+                        archivo: {
+                            $concat: ["$unidad", "_", "$numero_reporte", "_",
+                                { $first: { $map: { input: "$items", as: "r", in: { $toString: "$$r.tipo_actividad" } } } }, "_", "$tag", "_",
                                 {
-                                    $toString: {
-                                        $year: "$fecha_inspeccion"
+                                    $let: {
+                                        vars: { monthsInString: ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DIC'] },
+                                        in: { $arrayElemAt: ['$$monthsInString', { '$month': '$fecha_inspeccion' }] }
                                     }
                                 },
-                                "/",
-                                {
-                                    $toString: {
-                                        $isoWeek: "$fecha_inspeccion"
-                                    }
-                                }
+                                { $toString: { '$dayOfMonth': '$fecha_inspeccion' } }
                             ]
                         },
-                        'AAMM_inspeccion': {
-                            $concat: [
-                                {
-                                    $toString: {
-                                        $year: "$fecha_inspeccion"
-                                    }
-                                },
-                                "/",
-                                {
-                                    $toString: {
-                                        $month: "$fecha_inspeccion"
-                                    }
-                                }
-                            ]
-                        },
-                        'mes_inspeccion_word': 1,
-                        'año_inspeccion': {
-                            $toString: { '$year': '$fecha_inspeccion' }
-                        },
-                        'trabajo_terminado': 1,
-                        'trabajo_terminado_fecha': 1,
-                        'informe_realizado': 1,
-                        'informe_realizado_fecha': 1,
-                        'informe_revisado': 1,
-                        'informe_revisado_fecha': 1,
-                        'remito_realizado': 1,
-                        'remito_realizado_fecha': 1,
-                        'certificado_realizado': 1,
-                        'certificado_realizado_fecha': 1,
-                        'items.descripcion_servicio': 1,
-                        'items.codigo_servicio': 1,
-                        'items.clase': 1,
-                        'items.cantidad': 1,
-                        'items.cantidad': 1,
-                        'items.unidad_medida': 1,
-                        'items.tipo_actividad': 1,
-
+                        items_cantidad: { $first: { $map: { input: "$items", as: "r", in: { $toString: "$$r.cantidad" } } } }
                     }
                 },
-                // Se crea un nuevo campo con la cantidad del primer Ítem convertido a STR, para poder 
-                //realizar la comparación.
-                {
-                    $addFields: {
-                        'items_cantidad': {
-                            $first: {
-                                $map: {
-                                    input: "$items",
-                                    as: "r",
-                                    in: { $toString: "$$r.cantidad" }
-                                }
-                            }
-                        }
-                    }
-                },
+                { $unset: ['items.valor_total', 'items.valor_unitario'] },
                 {
                     '$match': {
                         $and: [
-                            { "numero_reporte": { $regex: search["numero_reporte"], $options: "i" } },
-                            { "tag": { $regex: search["tag"], $options: "i" } },
-                            { "items.0.descripcion_servicio": { $regex: search["items.0.descripcion_servicio"], $options: "i" } },
-                            { "items_cantidad": { $regex: search["items.0.cantidad"], $options: "i" } },
-                            { "inspector": { $regex: search["inspector"], $options: "i" } },
-                            { "numero_orden": { $regex: search["numero_orden"], $options: "i" } },
-                            { "cliente": { $regex: search["cliente"], $options: "i" } },
-                            { "contrato": { $regex: search["contrato"], $options: "i" } },
-                            { "unidad": { $regex: search["unidad"], $options: "i" } },
-                            /* { "fecha_carga": { $regex: search["fecha_carga"], $options: "i" } },*/
-                            { "semana_carga": { $regex: search["semana_carga"], $options: "i" } },
-                            /* { "fecha_inspeccion": { $regex: search["fecha_inspeccion"], $options: "i" } }, */
-                            { "AAMM_inspeccion": { $regex: search["AAMM_inspeccion"], $options: "i" } },
-                            { "AAsem_inspeccion": { $regex: search["AAsem_inspeccion"], $options: "i" } },
-                            { "semana_inspeccion": { $regex: search["semana_inspeccion"], $options: "i" } },
+                            { "numero_reporte": { $regex: req.query["numero_reporte"] || "", $options: "i" } },
+                            { "tag": { $regex: req.query["tag"] || "", $options: "i" } },
+                            { "items.0.descripcion_servicio": { $regex: req.query["items.0.descripcion_servicio"] || "", $options: "i" } },
+                            { "items.0.codigo_servicio": { $regex: req.query["items.0.codigo_servicio"] || "", $options: "i" } },
+                            { "items_cantidad": { $regex: req.query["items.0.cantidad"] || "", $options: "i" } },
+                            { "inspector": { $regex: req.query["inspector"] || "", $options: "i" } },
+                            { "numero_orden": { $regex: req.query["numero_orden"] || "", $options: "i" } },
+                            { "cliente": { $regex: req.query["cliente"] || "", $options: "i" } },
+                            { "contrato": { $regex: req.query["contrato"] || "", $options: "i" } },
+                            { "unidad": { $regex: req.query["unidad"] || "", $options: "i" } },
+                            /* { "fecha_carga": { $regex: req.query["fecha_carga"] || "", $options: "i" } },*/
+                            { "semana_carga": { $regex: req.query["semana_carga"] || "", $options: "i" } },
+                            /* { "fecha_inspeccion": { $regex: req.query["fecha_inspeccion"] || "", $options: "i" } }, */
+                            { "AAMM_inspeccion": { $regex: req.query["AAMM_inspeccion"] || "", $options: "i" } },
+                            { "AAsem_inspeccion": { $regex: req.query["AAsem_inspeccion"] || "", $options: "i" } },
+                            { "semana_inspeccion": { $regex: req.query["semana_inspeccion"] || "", $options: "i" } },
+                            { "archivo": { $regex: req.query["archivo"] || "", $options: "i" } },
 
                             //Campos Booleanos
-                            { "trabajo_terminado": { $in: search["trabajo_terminado"] } },
-                            { "informe_realizado": { $in: search["informe_realizado"] } },
-                            { "informe_revisado": { $in: search["informe_revisado"] } },
-                            { "remito_realizado": { $in: search["remito_realizado"] } },
-                            { "certificado_realizado": { $in: search["certificado_realizado"] } },
-
-                            { "items.0.codigo_servicio": { $regex: search["items.0.codigo_servicio"], $options: "i" } },
-                            /* { "archivo": { $regex: search["archivo"], $options: "i" } },   */
+                            { "trabajo_terminado": { $in: bTA.booleanToArray(req.query["trabajo_terminado"]) || [true, false] } },
+                            { "informe_realizado": { $in: bTA.booleanToArray(req.query["informe_realizado"]) || [true, false] } },
+                            { "informe_revisado": { $in: bTA.booleanToArray(req.query["informe_revisado"]) || [true, false] } },
+                            { "remito_realizado": { $in: bTA.booleanToArray(req.query["remito_realizado"]) || [true, false] } },
+                            { "certificado_realizado": { $in: bTA.booleanToArray(req.query["certificado_realizado"]) || [true, false] } },
                         ]
                     }
                 },
